20945f2cc1d8fb202cd4e4ebdebe83de8b011e5d
Nez <sanyapoom.siri@gmail.com>
2022-12-29 17:52:45 -0800

    read in json file and change footer img based on page

--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -13,6 +13,7 @@ namespace WiredBrainCoffeeAdmin.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<SurveyItem> SurveyResults { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -21,6 +22,9 @@ namespace WiredBrainCoffeeAdmin.Pages
 
         public void OnGet()
         {
+            var rawJson = System.IO.File.ReadAllText("wwwroot/sampledata/survey.json");
+            SurveyResults = JsonSerializer.Deserialize<List<SurveyItem>>(rawJson);
+
 
         }
     }
